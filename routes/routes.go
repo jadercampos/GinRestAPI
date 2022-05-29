6c692c95c2ae243420b5fc7b251eddef4a40376b
Jader Campos <jader.campos@gmail.com>
2022-05-29 20:32:06 -0300

    Implement HTTP routes

--- routes/routes.go
+++ routes/routes.go
@@ -7,7 +7,9 @@ import (
 
 func HandleRequests() {
 	r := gin.Default()
-	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/:nome", controllers.Saudacao)
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.Run(":5000")
 }
