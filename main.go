dd7d84dd75f5a5ba6fbbddb02fac9d86c2dada6c
Jader Campos <jader.campos@gmail.com>
2022-05-29 20:09:54 -0300

    add GORM and create connection

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/jadercampos/GinRestAPI/database"
 	"github.com/jadercampos/GinRestAPI/models"
 	"github.com/jadercampos/GinRestAPI/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Pedro", CPF: "0000000000", RG: "99999999"},
 		{Nome: "João", CPF: "1111111111", RG: "88888888"},
