77d7453469461f5da5c068c3b663d0734b84c0a4
Jader Campos <jader.campos@gmail.com>
2022-05-30 00:18:57 -0300

    Creating all test suite

--- main_test.go
+++ main_test.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jadercampos/GinRestAPI/controllers"
+	"github.com/jadercampos/GinRestAPI/database"
+	"github.com/jadercampos/GinRestAPI/models"
 	"github.com/stretchr/testify/assert"
 )
 
 func SetupDasRotasDeTeste() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	rotas := gin.Default()
 	return rotas
 }
@@ -26,4 +32,90 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	mockDaResposta := `{"API diz:":"E ai tchuco, tudo beleza?"}`
 	respostaBody, _ := ioutil.ReadAll(resposta.Body)
 	assert.Equal(t, mockDaResposta, string(respostaBody))
+}
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Erro, sumiram os alunos?")
+}
+
+func TestBuscaAlunoPorCPFHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/01234567890", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Erro, cade o  FDP desse CPF?")
+}
+
+func TestBuscaAlunoPorIDHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	pathDaBusca := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("GET", pathDaBusca, nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	var alunoMock models.Aluno
+	json.Unmarshal(resposta.Body.Bytes(), &alunoMock)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Erro, cade o  FDP desse ID?")
+	assert.Equal(t, alunoMock.Nome, "Aluno Teste da Silva", "Erro, o nome dele tá zoado")
+	assert.Equal(t, alunoMock.RG, "123456789", "Erro, o rg dele tá zoado")
+	assert.Equal(t, alunoMock.CPF, "01234567890", "Erro, o cpf dele tá zoado")
+}
+
+func TestDeletaAlunoHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.DELETE("/alunos/:id", controllers.DeletaAlunos)
+	pathParaDeletar := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("DELETE", pathParaDeletar, nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Erro, não consegui apagar o cara")
+}
+
+func TestEditaAlunoHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.PATCH("/alunos/:id", controllers.EditaAlunos)
+	aluno := models.Aluno{Nome: "Aluno Teste da Silva Sauro", RG: "000000000", CPF: "11111111111"}
+	valorJson, _ := json.Marshal(aluno)
+	pathParaEditar := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("PATCH", pathParaEditar, bytes.NewBuffer(valorJson))
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	var alunoMockAtualizado models.Aluno
+	json.Unmarshal(resposta.Body.Bytes(), &alunoMockAtualizado)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Erro, cade o  FDP desse ID?")
+	assert.Equal(t, alunoMockAtualizado.Nome, "Aluno Teste da Silva Sauro", "Erro, o nome dele tá zoado")
+	assert.Equal(t, alunoMockAtualizado.RG, "000000000", "Erro, o rg dele tá zoado")
+	assert.Equal(t, alunoMockAtualizado.CPF, "11111111111", "Erro, o cpf dele tá zoado")
+}
+
+var ID int
+
+func CriaAlunoMock() {
+	aluno := models.Aluno{Nome: "Aluno Teste da Silva", RG: "123456789", CPF: "01234567890"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
 }
