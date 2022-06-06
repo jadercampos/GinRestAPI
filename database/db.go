2323d8fd02212a1f51a3f8affe578593f7365029
Jader Campos <jader.campos@gmail.com>
2022-06-06 11:25:45 -0300

    Prepparing HGeroku deploy

--- database/db.go
+++ database/db.go
@@ -15,7 +15,7 @@ var (
 
 func ConectaComBancoDeDados() {
 	//stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
-	stringDeConexao := "host=ec2-3-211-221-185.compute-1.amazonaws.com user=dxgetmqtrczbmj password=807d01583250185ecba479ccd30de6f70466c6b2033bee18d4a90565a607b8d8 dbname=da0gk0gmlam0dq port=5432 sslmode=disable"
+	stringDeConexao := "host=ec2-3-211-221-185.compute-1.amazonaws.com user=dxgetmqtrczbmj password=807d01583250185ecba479ccd30de6f70466c6b2033bee18d4a90565a607b8d8 dbname=da0gk0gmlam0dq port=5432 sslmode=require"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
--- routes/routes.go
+++ routes/routes.go
@@ -4,24 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jadercampos/GinRestAPI/controllers"
 	docs "github.com/jadercampos/GinRestAPI/docs"
-	websocket "github.com/jadercampos/GinRestAPI/web-socket"
 	cors "github.com/rs/cors/wrapper/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func HandleRequests() {
+	docs.SwaggerInfo.BasePath = "/"
 	r := gin.Default()
-
-	// Enable CORS
 	r.Use(cors.Default())
-
-	// Static Content
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/assets", "./assets")
-	r.LoadHTMLFiles("ws.html")
-
-	// Rest API
 	r.GET("/:nome", controllers.Saudacao)
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
@@ -31,14 +24,12 @@ func HandleRequests() {
 	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.GET("/index", controllers.ExibePaginaIndex)
 	r.NoRoute(controllers.RotaNaoEncontrada)
-
-	// Swagger documentation
-	docs.SwaggerInfo.BasePath = "/"
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-
-	// Web Socket
-	r.GET("/wspage", func(c *gin.Context) { c.HTML(200, "ws.html", nil) })
-	r.GET("/ws", func(c *gin.Context) { websocket.WsHandler(c.Writer, c.Request) })
-
 	r.Run(":5000")
+
+	//r.LoadHTMLFiles("ws.html")
+	// r.GET("/wspage", func(c *gin.Context) { c.HTML(200, "ws.html", nil) })
+	// r.GET("/ws", func(c *gin.Context) { websocket.WsHandler(c.Writer, c.Request) })
+
+	// r.Run(":5001")
 }
