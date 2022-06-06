4fc467067d52a3f1e3e91b72b51ee55590a0b5da
Jader Campos <jader.campos@gmail.com>
2022-06-06 00:53:30 -0300

    refactoring

--- routes/routes.go
+++ routes/routes.go
@@ -1,24 +1,27 @@
 package routes
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"github.com/jadercampos/GinRestAPI/controllers"
 	docs "github.com/jadercampos/GinRestAPI/docs"
+	websocket "github.com/jadercampos/GinRestAPI/web-socket"
 	cors "github.com/rs/cors/wrapper/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func HandleRequests() {
-	docs.SwaggerInfo.BasePath = "/"
 	r := gin.Default()
+
+	// Enable CORS
 	r.Use(cors.Default())
+
+	// Static Content
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/assets", "./assets")
+	r.LoadHTMLFiles("ws.html")
+
+	// Rest API
 	r.GET("/:nome", controllers.Saudacao)
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
@@ -28,38 +31,14 @@ func HandleRequests() {
 	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.GET("/index", controllers.ExibePaginaIndex)
 	r.NoRoute(controllers.RotaNaoEncontrada)
+
+	// Swagger documentation
+	docs.SwaggerInfo.BasePath = "/"
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
-	r.LoadHTMLFiles("ws.html")
-
-	r.GET("/wspage", func(c *gin.Context) {
-		c.HTML(200, "ws.html", nil)
-	})
-
-	r.GET("/ws", func(c *gin.Context) {
-		wshandler(c.Writer, c.Request)
-	})
+	// Web Socket
+	r.GET("/wspage", func(c *gin.Context) { c.HTML(200, "ws.html", nil) })
+	r.GET("/ws", func(c *gin.Context) { websocket.WsHandler(c.Writer, c.Request) })
 
 	r.Run(":5000")
-}
-
-var wsupgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-func wshandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := wsupgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Printf("Failed to set websocket upgrade: %+v\n", err)
-		return
-	}
-
-	for {
-		t, msg, err := conn.ReadMessage()
-		if err != nil {
-			break
-		}
-		conn.WriteMessage(t, msg)
-	}
 }
---
+++ web-socket/ws.go
@@ -0,0 +1,29 @@
+package websocket
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func WsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Printf("Failed to set websocket upgrade: %+v\n", err)
+		return
+	}
+
+	for {
+		t, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		conn.WriteMessage(t, msg)
+	}
+}
